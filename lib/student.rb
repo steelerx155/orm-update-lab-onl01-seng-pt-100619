51f14ebfe5e1c6076dd860c9de8f0d17cd36d9dc
Randy Steele <steelerx155@gmail.com>
2019-12-20 23:23:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,7 @@ class Student
 
   def self.create_table 
     sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS students (
+    CREATE TABLE Isql = "DF NOT EXISTS students (
     id INTEGER PRIMARY KEY,
     name TEXT,
     grade TEXT
